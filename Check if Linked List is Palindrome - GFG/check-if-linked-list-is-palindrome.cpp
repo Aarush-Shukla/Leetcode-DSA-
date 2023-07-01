d5b75e6fcdfb840001e7d190d89cffd513c2a0d9
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2023-07-01 14:17:45 +0530

    Added solution - LeetHub

--- Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -28,34 +28,88 @@ struct Node {
   }
 };
 */
-#include<vector>
+// #include<vector>
 class Solution{
   public:
     //Function to check whether the list is palindrome.
     
-    bool checkPalindrome(vector<int> arr){
-        int n=arr.size();
-        int s=0;
-        int e=n-1;
-        while(s<=e){
-            if(arr[s]!=arr[e]){
-                return 0;
-            }
-            s++;
-            e--;
-            }
-            return 1;
-    }
+    // bool checkPalindrome(vector<int> arr){
+    //     int n=arr.size();
+    //     int s=0;
+    //     int e=n-1;
+    //     while(s<=e){
+    //         if(arr[s]!=arr[e]){
+    //             return 0;
+    //         }
+    //         s++;
+    //         e--;
+    //         }
+    //         return 1;
+    // }
+    
+   Node* reverse(Node* &head){
+       Node* curr=head;
+       Node* prev=NULL;
+       Node* forward=NULL;
+       
+       while(curr!=NULL){
+           forward=curr->next;
+           curr->next=prev;
+           prev=curr;
+           curr=forward;
+       }
+       return prev;
+   }
+   
+   Node* findMiddle(Node* &head){
+       Node* fast=head->next;
+       Node* slow=head;
+       
+       while(fast!=NULL&& fast->next!=NULL){
+           fast=fast->next->next;
+           slow=slow->next;
+       }
+       return slow;
+   }
     
     bool isPalindrome(Node *head)
     {
-        vector<int> arr;
-        Node* temp=head;
-        while(temp!=NULL){
-            arr.push_back(temp->data);
-            temp=temp->next;
+    //     vector<int> arr;
+    //     Node* temp=head;
+    //     while(temp!=NULL){
+    //         arr.push_back(temp->data);
+    //         temp=temp->next;
+    //     }
+    //     return checkPalindrome(arr);
+    
+    if(head==NULL|| head->next==NULL){
+        return true;
+    }
+    
+     //step1- find Midddle
+    Node* middle=findMiddle(head);
+    //step2- reverse the second part
+    Node* temp=middle->next;
+    middle->next=reverse(temp);
+    //step3- compare the two halves;
+    Node* head1=head;
+    Node* head2= middle->next;
+    
+    while(head2!=NULL){
+        if(head1->data!=head2->data){
+            return false;
         }
-        return checkPalindrome(arr);
+        head1=head1->next;
+        head2=head2->next;
+    }
+  
+    // //step4- repeat step 2 once again to reatin the original Linked List
+    // temp=middle->next;
+    // middle->next=reverse(temp);
+    
+    
+    return true;
+    
     }
 };
 
