bf417c8348b1ab0685b07858b8751945ee142cda
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2023-07-06 03:49:11 +0530

    Added solution - LeetHub

--- First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -29,49 +29,38 @@ int main() {
 // } Driver Code Ends
 
 
+typedef pair<int,int> pi;
+
 vector<long long> printFirstNegativeInteger(long long int A[],
                                              long long int N, long long int K) {
-         deque<long long int> dq;
-         vector<long long> ans;
-         int negative = -1;
-         
-         //process first window
-         for(int i=0; i<K; i++) {
-             if(A[i] < 0) {
-                 dq.push_back(i);
-             }
-         }
-         
-         //push ans for FIRST window
-         if(dq.size() > 0) {
-             ans.push_back(A[dq.front()]);
-         }
-         else
-         {
-             ans.push_back(0);
-         }
-         
-         //now process for remaining windows
-         for(int i = K; i<N; i++) {
-             //first pop out of window element
-             
-             
-             if(!dq.empty() && (i - dq.front())>=K ) {
-                 dq.pop_front();
-             }
-             
-             //then push current element
-             if(A[i] < 0)
-                dq.push_back(i);
-             
-            // put in ans
-            if(dq.size() > 0) {
-                 ans.push_back(A[dq.front()]);
-            }
-            else
-            {
-                ans.push_back(0);
-            }
-         }
-         return ans;
+                                                 
+                                                 
+    queue<pi>q;
+    vector<long long>ans;
+    
+    int i;
+    for( i=0; i<K-1; i++){
+        if(A[i]<0)q.push({A[i],i});
+    }
+    
+    pi p;
+    
+    int j =0;
+    while(i<N){
+        if(A[i]<0)q.push({A[i],i});
+        
+        if(q.empty())ans.push_back(0);
+        
+        else{
+            p = q.front();
+            ans.push_back(p.first);
+            
+            if(p.second<=j)q.pop();
+        }
+        j++;
+        i++;
+        
+    }
+    
+    return ans;
  }
