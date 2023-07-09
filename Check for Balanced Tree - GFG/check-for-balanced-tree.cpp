985714e905cf235c0dabae3f013fe09f1a61fbea
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2023-07-09 16:00:16 +0530

    Added solution - LeetHub

--- Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
+++ Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
@@ -104,32 +104,33 @@ struct Node
 class Solution{
     public:
     
-    int height(Node* node){
-        //base case
-        if(node==NULL){
-            return 0;
-        }
-        int left=height(node->left);
-        int right=height(node->right);
-        int ans=max(left,right)+1;
-        return ans;
+ pair<bool,int> isBalancedFast(Node *root){
+      pair<bool,int> ans;
+      //base case
+      if(root==NULL){
+           pair<bool,int> p= make_pair(true,0);
+          return p;
+      }
+      
+      pair<bool,int> left=isBalancedFast(root->left);
+      pair<bool,int> right=isBalancedFast(root->right);
+      
+      bool leftAns=left.first;
+      bool rightAns=right.first;
+      bool diff= abs(left.second-right.second)<=1;
+      ans.second=max(left.second,right.second)+1;
+      if(leftAns && rightAns && diff){
+          ans.first=true;
+      }else{
+          ans.first=false;
+      }
+      return ans;
     }
     
     //Function to check whether a binary tree is balanced or not.
     bool isBalanced(Node *root)
     {
-      //base case
-      if(root==NULL){
-          return true;
-      }
-      bool left=isBalanced(root->left);
-      bool right=isBalanced(root->right);
-      bool diff= abs(height(root->left)-height(root->right))<=1;
-      if(left && right && diff){
-          return true;
-      }else{
-          return false;
-      }
+      return isBalancedFast(root).first;
     }
 };
 
