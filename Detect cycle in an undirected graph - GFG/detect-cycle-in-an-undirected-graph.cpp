0b49279dad2005c5bd87425b95aefad623da7fc3
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2023-07-21 22:00:23 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -6,22 +6,38 @@ using namespace std;
 class Solution {
   public:
     
-    bool isCycle(int src,int V, vector<int> adj[],int vis[]){
-        queue <pair<int,int>> q;
-        vis[src]=1;
-        q.push({src,-1});
-        while(!q.empty()){
-            int node=q.front().first;
-            int parent=q.front().second;
-            q.pop();
+    // bool isCycle(int src,int V, vector<int> adj[],int vis[]){
+    //     queue <pair<int,int>> q;
+    //     vis[src]=1;
+    //     q.push({src,-1});
+    //     while(!q.empty()){
+    //         int node=q.front().first;
+    //         int parent=q.front().second;
+    //         q.pop();
             
-            for(auto i:adj[node]){
-                if(!vis[i]){
-                    vis[i]=1;
-                    q.push({i,node});
-                }else if(i!=parent){
+    //         for(auto i:adj[node]){
+    //             if(!vis[i]){
+    //                 vis[i]=1;
+    //                 q.push({i,node});
+    //             }else if(i!=parent){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+    
+    
+    bool isCycle_DFS(int node,int parent,int vis[],vector<int> adj[] ){
+        vis[node]=1;
+        for(auto i:adj[node]){
+            if(!vis[i]){
+                if(isCycle_DFS(i,node,vis,adj)){
                     return true;
                 }
+            }
+            else if(i!=parent) { // iss condition mai viited hai i aur vo parent nahi hai iska matlab cycle hai
+                return true;
             }
         }
         return false;
@@ -30,14 +46,27 @@ class Solution {
     
     // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
+        // int vis[V]={0};
+        // for(int i=0;i<V;i++){
+        //     if(!vis[i]){
+        //         if(isCycle(i,V,adj,vis)){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        
+        
+        
         int vis[V]={0};
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                if(isCycle(i,V,adj,vis)){
-                    return true;
-                }
+            if(isCycle_DFS(i,-1,vis,adj)){
+                return true;
             }
         }
+        }
+        
         return false;
     }
 };
