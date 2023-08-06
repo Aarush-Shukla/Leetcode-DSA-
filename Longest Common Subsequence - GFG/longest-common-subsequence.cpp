7efe1da2d4f58a1e81cb066ec696f85cf21a893a
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2023-08-06 09:24:49 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -28,7 +28,7 @@ for(int i=0;i<n+1;i++){
 //Choice Diagram
 
 for(int i=1;i<n+1;i++){
-    for(int j=0;j<m+1;j++){
+    for(int j=1;j<m+1;j++){
 
         if(s1[i-1]==s2[j-1]){
         dp[i][j] = 1+dp[i-1][j-1];
