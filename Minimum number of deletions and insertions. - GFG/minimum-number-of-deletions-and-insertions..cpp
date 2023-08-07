0d81717d5359b34ef72fc040e61ac1255e671235
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2023-08-07 14:18:33 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+
+	public:
+
+int lcs(int n, int m, string s1, string s2){
+    n=s1.length();
+    m=s2.length();
+
+    vector<vector<int>> dp(n+1,vector<int>(m+1));
+ 
+    //Base Case
+
+for(int i=0;i<n+1;i++){
+    for(int j=0;j<m+1;j++){
+
+        dp[i][0]=0;
+        dp[0][j]=0;
+
+    }
+}
+
+//Choice Diagram
+
+for(int i=1;i<n+1;i++){
+    for(int j=1;j<m+1;j++){
+
+        if(s1[i-1]==s2[j-1]){
+        dp[i][j] = 1+dp[i-1][j-1];
+        }
+         else{
+        dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+}
+    
+      return dp[n][m];
+
+}
+
+	int minOperations(string str1, string str2) 
+	{ 
+	    int n=str1.length();
+	    int m=str2.length();
+	    
+	    int deletion= n-lcs(n,m,str1,str2);
+	    int insertion= m-lcs(n,m,str1,str2);
+	    
+	    return deletion+insertion;
+	} 
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+	    Solution ob;
+	    cout << ob.minOperations(s1, s2) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
