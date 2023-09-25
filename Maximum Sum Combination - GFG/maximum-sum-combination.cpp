e29a310a1b609d8a5b3fa0960a4962f4e22c2aa8
Aarush-Shukla <98807835+Aarush-Shukla@users.noreply.github.com>
2023-09-25 21:40:59 +0530

    Added solution - LeetHub

---
+++ Maximum Sum Combination - GFG/maximum-sum-combination.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> maxCombinations(int N, int K, vector<int> &A, vector<int> &B) {
+             // first sort a both array
+        sort(A.begin(), A.end());
+        sort(B.begin(), B.end());
+        
+        // create a priority queue of max heap
+        priority_queue<int> pq;
+        
+        for(int i = N-1 ; i >= 0 ; i--){
+            for(int j = N-1 ; j >= 0 ; j--){
+                // insert a negative element for mean heap
+                int sum = (A[i] + B[j]) * -1;
+                if(pq.size() < K){
+                    pq.push(sum);
+                }else if(sum < pq.top()){
+                    pq.pop();
+                    pq.push(sum);
+                }else break;
+            }
+        }
+       // create a vector of size k
+        vector<int> result(K--);
+
+        // geting value in vector
+        while(!pq.empty()){
+            // make value positive at their position
+            result[K--] = pq.top()*-1;
+            pq.pop();
+        }
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+
+        vector<int> A(N), B(N);
+        for (int i = 0; i < N; i++) {
+            cin >> A[i];
+        }
+        for (int i = 0; i < N; i++) {
+            cin >> B[i];
+        }
+        Solution obj;
+        vector<int> ans = obj.maxCombinations(N, K, A, B);
+        for (auto &it : ans) cout << it << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
